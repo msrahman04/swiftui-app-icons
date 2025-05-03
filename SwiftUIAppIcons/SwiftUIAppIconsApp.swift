ffdb90f15f063c10d04ea605c8d8c57059fed795
Shahin <msrahman.mail@gmail.com>
2025-05-03 21:56:47 +0600

    Initial Commit

---
+++ SwiftUIAppIcons/SwiftUIAppIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppIconsApp.swift
+//  SwiftUIAppIcons
+//
+//  Created by Shahinur Rahman on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIAppIconsTests/SwiftUIAppIconsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppIconsTests.swift
+//  SwiftUIAppIconsTests
+//
+//  Created by Shahinur Rahman on 3/5/25.
+//
+
+import Testing
+@testable import SwiftUIAppIcons
+
+struct SwiftUIAppIconsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
